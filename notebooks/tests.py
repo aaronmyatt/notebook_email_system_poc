40d622f596b34b82b243d138855f3945c8a5a890
Aaron Myatt <aaronmyatt@gmail.com>
2018-02-19 23:09:04 +0800

    test notebook list view

--- notebooks/tests.py
+++ notebooks/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from mock import patch
+from . import views
 
-# Create your tests here.
+
+class TestNotebooksListView:
+    
+    def test_notebooks_view(self, rf):
+        request = rf.get('/notebooks')
+        view = views.all_notebooks
+        response = view(request)
+        assert response.status_code == 200
+
+    @patch('notebooks.views.render')
+    def test_view_passes_list_of_paths_to_context(self, mock_render, rf):
+        request = rf.get('/notebooks')
+        view = views.all_notebooks
+        response = view(request)
+        assert mock_render.called
+        assert len(mock_render.call_args[1]['context']['file_paths']) > 0
