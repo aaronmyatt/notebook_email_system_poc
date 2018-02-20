2ef3b3653964019aa2e3f16d28340fc95e1f7b0b
Aaron Myatt <aaronmyatt@gmail.com>
2018-02-20 23:28:50 +0800

    move primary logic to utils

--- notebooks/decorators.py
+++ notebooks/decorators.py
@@ -1,13 +1,10 @@
-import glob
-import os
 import functools
-from django.conf import settings
+from .utils import file_paths_in_project_dir
 
 def notebook_paths(f):
     @functools.wraps(f)
     def wrapper(request, *args, **kwargs):
-        notebook_path = os.path.join(settings.BASE_DIR, 'notebooks/input/*')
-        file_paths = glob.glob(notebook_path)
-        kwargs.update(dict(file_paths=file_paths))
+        notebook_paths = file_paths_in_project_dir('notebooks/input/')
+        kwargs.update(dict(file_paths=notebook_paths))
         return f(request, *args, **kwargs)
     return wrapper
