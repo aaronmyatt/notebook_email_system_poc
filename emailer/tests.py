5871e07897582316749813bfcbf554c936f1237d
Aaron Myatt <aaronmyatt@gmail.com>
2018-02-23 00:06:51 +0800

    send emails to only NR users who havent recieved email in 3 days

--- emailer/tests.py
+++ emailer/tests.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 import pytest
 from mock import patch
 from django.test import TestCase
@@ -9,13 +10,20 @@ from . import sender
 
 seeder = Seed.seeder()
 
-def make_users(User):
+TODAY = date.today()
+FOUR_DAYS_AGO = (date.today() - timedelta(4))
+TWO_DAYS_AGO = (date.today() - timedelta(2))
+
+def make_users(User, state='A', last_email=TODAY):
     seeder.add_entity(User, 3)
     seeder.execute()
     u = User.objects.first()
     s = u.activity.all()[0]
-    s.state = 'I'
+    s.state = state
+    e = u.emails.all()[0]
+    e.last_email = last_email
     s.save()
+    e.save()
 
 class TestUserModelSignal:
 
@@ -27,10 +35,34 @@ class TestUserModelSignal:
 
 class TestEmailSender:
 
+
+    class TestInactiveUsers:
+
+        @patch('emailer.sender.send_emails')
+        def test_does_not_recieve_email(self, mock_sender, django_user_model, db):
+            make_users(django_user_model, state='I')
+            sender.sender(django_user_model)
+            assert mock_sender.called
+            inactive_count = mock_sender.call_args[0][0].filter(activity__state='I').count()
+            assert inactive_count == 0
+
+
     @patch('emailer.sender.send_emails')
-    def test_does_not_email_inactive_users(self, mock_sender, django_user_model, db):
-        make_users(django_user_model)
-        sender.sender(django_user_model)
-        assert mock_sender.called
-        inactive_count = mock_sender.call_args[0][0].filter(activity__state='I').count()
-        assert inactive_count == 0
+    class TestNonResponsiveUsers:
+
+        def test_gets_email_after_three_days(self, mock_sender, django_user_model, db):
+            make_users(django_user_model, state='NR', last_email=FOUR_DAYS_AGO)
+            sender.sender(django_user_model)
+            assert mock_sender.called
+            nr_count = mock_sender.call_args[0][0].filter(activity__state='NR').count()
+            assert nr_count == 1
+
+        def test_exluded_when_last_email_too_recent(self, mock_sender, django_user_model, db):
+            make_users(django_user_model, state='NR', last_email=TWO_DAYS_AGO)
+            sender.sender(django_user_model)
+            assert mock_sender.called
+            nr_count = mock_sender.call_args[0][0].filter(activity__state='NR').count()
+            assert nr_count == 0
+
+
+
