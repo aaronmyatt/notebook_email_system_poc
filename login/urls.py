d676c5f0174c43b750018346526e6a8ed6808f32
Aaron Myatt <aaronmyatt@gmail.com>
2018-02-26 14:32:40 +0800

    map root to login view

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path(r'', views.LoginView.as_view(), name='home'),
     path(r'login/', views.LoginView.as_view(), name='login')
 ]
